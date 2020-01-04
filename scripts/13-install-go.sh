c615b1f0e1db4ef469f01adda8271d3ca8f84f6f
Marcin Dulak <Marcin.Dulak@gmail.com>
2020-01-04 05:59:46 +0100

    remove 13-install-go debugging

--- scripts/13-install-go.sh
+++ scripts/13-install-go.sh
@@ -6,28 +6,18 @@ PrepareInstallation() {
   sudo apt-get -qq -y update
   sudo apt-get -qq -y install cmake build-essential git autoconf automake \
     libtool curl build-essential
-  echo "end"
 }
 
 InstallGolang () {
-  echo "start1"
   export GOPATH=${HOME}/go
   GO_PACKAGE=go${LATEST_GO_VERSION}.linux-amd64.tar.gz
   
   mkdir -p ${GOPATH}
-  ls -al $HOME
-  ls -al /usr/local/go
-  go version
-  which go
   sudo rm -rf /usr/local/go/
-  ls -al /usr/local/go
-  echo "start2"
-  curl https://storage.googleapis.com/golang/${GO_PACKAGE} | sudo tar -C /usr/local -xz
-  ls -al /usr/local/go
+  curl -s https://storage.googleapis.com/golang/${GO_PACKAGE} | sudo tar -C /usr/local -xz
   sudo sh -c "echo 'export GOPATH=$GOPATH' >> /etc/profile"
   sudo sh -c "echo 'export PATH=/usr/local/go/bin:${GOPATH}/bin:$PATH' >> /etc/profile"
   sudo sh -c "echo 'export GOROOT=/usr/local/go' >> /etc/profile"
-  echo "start3"
 
   export GOPATH=$GOPATH
   export PATH=/usr/local/go/bin:${GOPATH}/bin:$PATH
