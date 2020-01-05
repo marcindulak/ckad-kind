b23bfcf0e57b0fa375e949c6fa091a665dca9721
Marcin Dulak <Marcin.Dulak@gmail.com>
2020-01-05 10:07:19 +0100

    switch order of local registry tests

--- scripts/24-test-ckad-registry.sh
+++ scripts/24-test-ckad-registry.sh
@@ -11,9 +11,9 @@ docker exec -it ckad-registry-control-plane bash -c "cat /etc/containerd/config.
 docker exec -it ckad-registry-control-plane bash -c "containerd -v"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-registry
 docker exec -it ckad-registry-control-plane bash -c "cat /etc/hosts | grep registry"
-# see https://github.com/containerd/cri/issues/1367
-docker exec -it ckad-registry-control-plane bash -c "ctr image pull --plain-http registry:5000/hello-app:1.0"
 kubectl create deployment hello-server --image=registry:5000/hello-app:1.0
-sleep 30
+sleep 10
 kubectl get pod -l app=hello-server | grep Running
 kubectl delete deployment hello-server
+# see https://github.com/containerd/cri/issues/1367
+docker exec -it ckad-registry-control-plane bash -c "ctr image pull --plain-http registry:5000/hello-app:1.0"
